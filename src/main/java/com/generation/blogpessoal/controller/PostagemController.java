901eaeabc1704fff71c1b2f11c162419c804c5d8
Rafaela <rafaela.imiani@outlook.com>
2025-10-22 22:23:36 -0300

    �✨ Implementação do Recurso Usuário e Spring Security ✨�

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -30,6 +31,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -54,14 +58,29 @@ public class PostagemController {
 	
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		
+		if(temaRepository.existsById(postagem.getTema().getId())) {
+			
 		postagem.setId(null);
 		
 	 return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		}
+	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O tema não existe!", null); 
 	}		
 	
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
 		
+		if(postagemRepository.existsById(postagem.getId())) {
+			if(temaRepository.existsById(postagem.getTema().getId())) {	
+               return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+			}
+			     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O tema não existe");
+	
+				
+			 
+			
+		}
 		 return postagemRepository.findById(postagem.getId())
 				   .map(resposta -> ResponseEntity.status(HttpStatus.OK)
 						   .body(postagemRepository.save(postagem)))
--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,28 +1,35 @@
 package com.generation.blogpessoal.model;
 
-
-
+import java.util.List; // ✅ importação correta
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name= "tb_temas") // CREATE TABLE tb_tema (
+@Table(name= "tb_temas")
 public class Tema {
 	
-	@Id // PRIMARY KEY (id)
-	@GeneratedValue(strategy= GenerationType.IDENTITY) // AUTO_INCREMENT
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(length = 200)
 	@NotBlank(message = "O atributo descrição é obrigatório!")
-	@Size(min = 100, max = 200, message = "O atributo descrição deve conter no mínimo 100 e no máximo 200 caracters")
+	@Size(min = 100, max = 200, message = "O atributo descrição deve conter no mínimo 100 e no máximo 200 caracteres")
 	private String descricao;
 	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties(value = "tema", allowSetters = true)
+	private List<Postagem> postagem;
 	
 	public Long getId() {
 		return this.id;
@@ -38,6 +45,14 @@ public class Tema {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 }
 
